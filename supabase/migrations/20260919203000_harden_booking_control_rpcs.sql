2e6db4b768b1aca3e42d16ed4e3b5b2be22c36f2
Slogan-creative <andrea.giglio@slogancreative.com>
2026-09-19 20:28:49 +0200

    Harden custom booking RPC permissions

---
+++ supabase/migrations/20260919203000_harden_booking_control_rpcs.sql
@@ -0,0 +1,3 @@
+revoke execute on function public.prenow_owner_set_customer_bookings(text,boolean) from anon;
+revoke execute on function public.prenow_staff_slots_custom(uuid,uuid,date,integer) from anon;
+revoke execute on function public.prenow_staff_book_custom(uuid,uuid,timestamptz,uuid,text,text,integer,integer) from anon;
